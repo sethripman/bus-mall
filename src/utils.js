1c9e1beb8af9d3b96ac1be46b1dfa60a41a01574
sethripman <sethripman@gmail.com>
2019-10-15 11:21:49 -0700

    wrote generate choices function;

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,29 @@ export const storePreviousDisplay = (firstId, secondId, thirdId) => {
     const previousDisplay = [firstId, secondId, thirdId];
     return previousDisplay;
 };
+
+// Gets three random items, compares for duplicates, compares against previous displayed choices, returns array of three non-dupe items
+export const generateChoices = (itemData, previousDisplayArray) => {
+    let choiceOne = getRandomItem(itemData);
+    let choiceTwo = getRandomItem(itemData); 
+    let choiceThree = getRandomItem(itemData);
+
+    let choiceArray;
+
+    // Validate Choices
+    while (choiceOne.id === previousDisplayArray[0] || choiceOne.id === previousDisplayArray[1] || choiceOne.id === previousDisplayArray[2]){
+        choiceOne = getRandomItem(itemData);
+    }
+
+    while (choiceTwo.id === previousDisplayArray[0] || choiceTwo.id === previousDisplayArray[1] || choiceTwo.id === previousDisplayArray[2] || choiceOne === choiceTwo){
+        choiceTwo = getRandomItem(itemData);
+    }
+
+    while (choiceThree.id === previousDisplayArray[0] || choiceThree.id === previousDisplayArray[1] || choiceThree.id === previousDisplayArray[2] || choiceOne === choiceThree || choiceTwo === choiceThree){
+        choiceThree = getRandomItem(itemData);
+    }
+
+    // assign validated choices to an array and return the array
+    choiceArray = [choiceOne, choiceTwo, choiceThree];
+    return choiceArray;
+};
