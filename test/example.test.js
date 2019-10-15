157e13bd41ef6106d686903d259b11b26d189591
sethripman <sethripman@gmail.com>
2019-10-15 11:48:29 -0700

    wrote test for display function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,23 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import { storePreviousDisplay, generateChoices } from '../src/utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('stores previous display into an array of ids', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const testInputOne = 'A';
+    const testInputTwo = 'B';
+    const testInputThree = 'C';
+
+    const expected = ['A', 'B', 'C'];
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const output = storePreviousDisplay(testInputOne, testInputTwo, testInputThree);
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.deepEqual(output, expected);
 });
