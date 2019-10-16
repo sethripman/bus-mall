a6f1afe1ba4ebb6b1ee2942229bae404dea29c3d
sethripman <sethripman@gmail.com>
2019-10-16 10:49:39 -0700

    reset generator array for next turn

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { itemData } from './src/api.js';
-import { generateChoice, storeDisplay, storeClick, displayChoice } from '../src/utils.js';
+import { generateChoice, storeDisplay, storeClick, displayChoice, removeById } from '../src/utils.js';
 
 // DOM Elements declaration
 const radioInputs = document.querySelectorAll('input[name=choice]');
@@ -9,7 +9,7 @@ const choiceFields = document.querySelectorAll('.choice');
 let generatorArray = itemData;
 let currentChoicesArray = [];
 let clickedDisplayArray = [];
-let turns = 1;
+let turns = 0;
 
 // On page load, fill current choices array with three items spliced from generator array and store display
 for (let i = 0; i < 3; i++) {
@@ -32,84 +32,23 @@ const handleRadioButtonClick = (event) => {
     const selected = event.target.value;
     storeClick(clickedDisplayArray, selected);
     turns++;
-    debugger;
+    
+    // reset generator for next showing
+    generatorArray = itemData;
+    for (let i = 0; i < 3; i++) {
+        removeById(generatorArray, currentChoicesArray[i].id);
+    }
+    currentChoicesArray = [];
+
+    for (let i = 0; i < 3; i++) {
+        const thisChoice = generateChoice(generatorArray);
+        currentChoicesArray.push(thisChoice);
+        storeDisplay(clickedDisplayArray, thisChoice.id);
+        displayChoice(choiceFields[i], currentChoicesArray[i]);
+    }
 };
 
 // Add event listeners to all buttons
 radioInputs.forEach((radioInput) => {
     radioInput.addEventListener('click', handleRadioButtonClick);
 });
-
-/* Event listener logic
-let correctAnswer;
-
-const handleRadioButtonClick = (event) => {
-    if (event.target.value === correctAnswer.id) {
-        myWins++;
-        trees.removeTreeById(event.target.value);
-    } else {
-        alert('boooo');
-    }
-};
-
-treeRadioTags.forEach((radioTag) => {
-    radioTag.addEventListener('click', handleRadioButtonClick);
-});
-
-const radioInputs = document.querySelectorAll('input[name=tree]');
-
-@@@@@@@@ THIS IS THE EVENT LISTENER ADDER @@@@@@@@@
-
-for (let i = 0; i < radioInputs.length, i++) {
-    const radioInput = radioInputs[i];
-    
-    radioInput.addEventListener('input',(event) => {
-        if (!live) return;
-
-        const userIsCorrect = correctTree.id === event.target.value;
-
-        change css
-    
-        turns++;
-    });
-}
-
-
-
-function handleUserChoice (event) {
-    if (!live) return;
-
-    const userIsCorrect = correctTree.id === event.target.value;
-    
-    turns++;
-    trackMisId(something.Id);
-}
-
-Then iterate through radio inputs, for each radio input, add event listener, update display, add a turn, redisplay
-
-
-
-function trackMissId(treeId) {
-    const found = findByID(misIds, treeID);
-    if (found) {
-        found.misses++;
-        return;
-    }
-
-    const misId = { id: treeId, misses: 1 }
-    misIds.push(misId);
-}
-
-LAST TURN DISPLAY function takes turns as param
-if (turns === 25) {
-    for (let i = 0; i < misIds.length, i++) {
-        const misId = misIds[i];
-        const tree = findById(trees, misId.id);
-        const dom = renderRow(misId, tree);
-        resultList.appendChild(dom);
-    }
-}
-
-resultList is a ul populated with dynamically generated li's
-
-*/
