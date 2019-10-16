96068ea193076e1672b48c98e71f390d65e4dde2
sethripman <sethripman@gmail.com>
2019-10-15 17:26:08 -0700

    wrote display/click functions

--- src/utils.js
+++ src/utils.js
@@ -27,34 +27,19 @@ export const generateChoice = (generatorArray) => {
     return newChoice;
 };
 
-//generates and returns an array of the current three IDs to be stored locally / compared against for dupes - deprecated, new data model used
-export const storePreviousDisplay = (firstId, secondId, thirdId) => {
-    const previousDisplay = [firstId, secondId, thirdId];
-    return previousDisplay;
+export const storeDisplay = (clickedDisplayArray, itemId) => {
+    const found = findById(clickedDisplayArray, itemId);
+    if (found) {
+        found.displayed++;
+        return;
+    }
+
+    const displayedItem = { id: found.id, displayed: 1, clicked: 0 };
+    clickedDisplayArray.push(displayedItem);
 };
 
-// Gets three random items, compares for duplicates, compares against previous displayed choices, returns array of three non-dupe items - deprecated, new data model used
-export const generateChoices = (itemData, previousDisplayArray) => {
-    let choiceOne = getRandomItem(itemData);
-    let choiceTwo = getRandomItem(itemData); 
-    let choiceThree = getRandomItem(itemData);
-
-    let choiceArray;
-
-    // Validate Choices
-    while (choiceOne.id === previousDisplayArray[0] || choiceOne.id === previousDisplayArray[1] || choiceOne.id === previousDisplayArray[2]){
-        choiceOne = getRandomItem(itemData);
-    }
-
-    while (choiceTwo.id === previousDisplayArray[0] || choiceTwo.id === previousDisplayArray[1] || choiceTwo.id === previousDisplayArray[2] || choiceOne === choiceTwo){
-        choiceTwo = getRandomItem(itemData);
-    }
-
-    while (choiceThree.id === previousDisplayArray[0] || choiceThree.id === previousDisplayArray[1] || choiceThree.id === previousDisplayArray[2] || choiceOne === choiceThree || choiceTwo === choiceThree){
-        choiceThree = getRandomItem(itemData);
-    }
-
-    // assign validated choices to an array and return the array
-    choiceArray = [choiceOne, choiceTwo, choiceThree];
-    return choiceArray;
+// Anything clicked must have been displayed, so no need for found logic
+export const storeClick = (clickedDisplayArray, itemId) => {
+    const found = findById(clickedDisplayArray, itemId);
+    found.clicked++;
 };
