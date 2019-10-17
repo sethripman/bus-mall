d3de7be29e0c602239f3d7fb7a50c0c6d23bca24
sethripman <sethripman@gmail.com>
2019-10-16 18:04:00 -0700

    working on chart

--- app.js
+++ app.js
@@ -56,24 +56,68 @@ const endGame = (turns) => {
         setChoices(clickedDisplayArray);
         let choices = getChoices();
         alert(JSON.stringify(choices, true, 2));
-    }
-    
-    // adding chart generation
-    
-    const labels = [getChartLabels(clickedDisplayArray)];
-    const data = [getChartDisplayData(clickedDisplayArray), getChartClickData()];
 
-    new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: '# of Votes',
-                data: data,
-                backgroundColor: ['cyan', 'magenta']
-            }]
-        },
-    });
+        const labels = [getChartLabels(clickedDisplayArray)];
+        const dataOne = [getChartDisplayData(clickedDisplayArray)];
+        const dataTwo = [getChartClickData(clickedDisplayArray)];
+
+        var
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '#',
+                    data: data,
+                    backgroundColor: ['cyan', 'magenta']
+                }]
+            },
+        });
+    }
+
+    /*
+    var densityData = {
+  label: 'Density of Planet (kg/m3)',
+  data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
+  backgroundColor: 'rgba(0, 99, 132, 0.6)',
+  borderWidth: 0,
+  yAxisID: "y-axis-density"
+};
+
+var gravityData = {
+  label: 'Gravity of Planet (m/s2)',
+  data: [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
+  backgroundColor: 'rgba(99, 132, 0, 0.6)',
+  borderWidth: 0,
+  yAxisID: "y-axis-gravity"
+};
+
+var planetData = {
+  labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+  datasets: [densityData, gravityData]
+};
+
+var chartOptions = {
+  scales: {
+    xAxes: [{
+      barPercentage: 1,
+      categoryPercentage: 0.6
+    }],
+    yAxes: [{
+      id: "y-axis-density"
+    }, {
+      id: "y-axis-gravity"
+    }]
+  }
+};
+
+var barChart = new Chart(densityCanvas, {
+  type: 'bar',
+  data: planetData,
+  options: chartOptions
+});
+    */
 };
 
 // On click function
